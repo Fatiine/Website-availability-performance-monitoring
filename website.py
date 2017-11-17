e339302e567344ad0c9a32a12450e0f327b392ee
Fatine BOUJNOUNI <fatine.boujnouni@gmail.com>
2017-11-17 02:19:23 +0100

    class website : update function get_stats and checkAlerts

--- website.py
+++ website.py
@@ -64,42 +64,162 @@ class Website():
 
         insert_values(database_name, "monitoring_table", values)
 
-    def get_stats(self, timeframe, database_name, stats_display_time):
-        stats_thread = threading.Timer(stats_display_time, self.get_stats, args=[timeframe,database_name, stats_display_time])
-        stats_thread.start()
-        os.system('clear')
-        # Compute the statistiques of some metrics over a timeframe
-        # print("Stats of the website : ", self.URL, "over ", timeframe/60, "minutes ")
-        printStr = '\n\033[1;30m ####### Statistiques of the website {} ########\033[0m'.format(self.URL)
+    # def get_stats(self, timeframe, database_name,stats_display_time):
+    #     stats_thread = threading.Timer(10, self.get_stats, args=[timeframe,database_name, stats_display_time])
+    #     stats_thread.start()
+    #     #os.system('clear')
+    #     # Compute the statistiques of some metrics over a timeframe
+    #     # print("Stats of the website : ", self.URL, "over ", timeframe/60, "minutes ")
+    #     printStr = '\n\033[4;93m ####### Statistiques of the website {} ########\033[0m'.format(self.URL)
 
+    #     now = datetime.datetime.now()
+    #     time_frame = datetime.timedelta(0, timeframe)
+    #     t = now - time_frame
+
+    #     selectData = (self.URL, t)
+
+    #     query_result = select_values(database_name, "monitoring_table", selectData)
+
+    #     if query_result is None:
+    #         printStr += "No data available"
+    #         return False, {} # "No data available"
+    #         '''False, {}'''
+
+    #     availabilities = Counter([element[2] for element in query_result if element[2] is not None])
+    #     status_code = Counter([element[3] for element in query_result])
+    #     response_times = [element[4] for element in query_result if element[4] is not None]
+
+    #     availability = availabilities[True] / sum(availabilities.values())
+    #     max_RT = max(response_times, default=float('inf'))
+    #     min_RT = min(response_times)
+    #     avg_RT = sum(response_times) / len(response_times)
+    #     time_date = datetime.datetime.now()
+    #     # print("availabilities : ", availabilities)
+    #     # print("availabilities[True] :", availabilities[True])
+    #     # print("availability :", availability)
+
+    #     if timeframe == 120: # 2 minutes
+    #         if availability < 0.8 and self.isDown == False:
+    #             self.isDown = True
+    #             messageType = "alert"
+
+    #             values = (self.URL, time_date, availability, self.isDown, messageType)
+    #             insert_values(database_name, "alerts_table", values)
+
+    #         if availability >= 0.8 and self.isDown == True:
+    #             self.isDown = False
+    #             messageType = "recovery"
+
+    #             values = (self.URL, time_date, availability, self.isDown, messageType)
+    #             insert_values(database_name, "alerts_table", values)
+
+    #     values = (self.URL, time_date, timeframe, availability, str(status_code.most_common()), max_RT, min_RT, avg_RT)
+    #     insert_values(database_name, "stats_table", values)
+
+
+    #     printStr += '\n\033[4;36m Past {} minutes:\033[0m'.format(timeframe/60) + \
+    # '\n\tMax/Avg/Min response time: {:.4f}/{:.4f}/{:.4f} s'.format(max_RT, avg_RT, min_RT) + \
+    # '\n\tResponse counts: {}'.format(status_code.most_common())+ '\n\tAvailability: {} %'.format(availability*100)
+
+    #     print(printStr)
+    #     return printStr
+    #     #True, printStr
+    #     '''{'Availability': availability, 'statusCodes': status_code.most_common(), 'maxRT': max_RT,
+    #                   'minRT:': min_RT, 'avgRT': avg_RT}'''
+
+    # def get_stats(self, timeframe, database_name,stats_display_time , printStr):
+    #     stats_thread = threading.Timer(10, self.get_stats, args=[timeframe,database_name, stats_display_time])
+    #     stats_thread.start()
+    #     #os.system('clear')
+    #     # Compute the statistiques of some metrics over a timeframe
+    #     now = datetime.datetime.now()
+    #     time_frame = datetime.timedelta(0, timeframe)
+    #     t = now - time_frame
+
+    #     selectData = (self.URL, t)
+
+    #     query_result = select_values(database_name, "monitoring_table", selectData)
+
+    #     if query_result is None:
+    #         printStr += "No data available"
+    #         return False, {} # "No data available"
+    #         '''False, {}'''
+
+    #     availabilities = Counter([element[2] for element in query_result if element[2] is not None])
+    #     status_code = Counter([element[3] for element in query_result])
+    #     response_times = [element[4] for element in query_result if element[4] is not None]
+
+    #     availability = availabilities[True] / sum(availabilities.values())
+    #     max_RT = max(response_times, default=float('inf'))
+    #     min_RT = min(response_times)
+    #     avg_RT = sum(response_times) / len(response_times)
+    #     time_date = datetime.datetime.now()
+    #     # print("availabilities : ", availabilities)
+    #     # print("availabilities[True] :", availabilities[True])
+    #     # print("availability :", availability)
+
+    #     if timeframe == 120: # 2 minutes
+    #         if availability < 0.8 and self.isDown == False:
+    #             self.isDown = True
+    #             messageType = "alert"
+
+    #             values = (self.URL, time_date, availability, self.isDown, messageType)
+    #             insert_values(database_name, "alerts_table", values)
+
+    #         if availability >= 0.8 and self.isDown == True:
+    #             self.isDown = False
+    #             messageType = "recovery"
+
+    #             values = (self.URL, time_date, availability, self.isDown, messageType)
+    #             insert_values(database_name, "alerts_table", values)
+
+    #     values = (self.URL, time_date, timeframe, availability, str(status_code.most_common()), max_RT, min_RT, avg_RT)
+    #     insert_values(database_name, "stats_table", values)
+
+
+    #     printStr += '\n\033[4;36m Past {} minutes:\033[0m'.format(timeframe/60) + \
+    # '\n\tMax/Avg/Min response time: {:.4f}/{:.4f}/{:.4f} s'.format(max_RT, avg_RT, min_RT) + \
+    # '\n\tResponse counts: {}'.format(status_code.most_common())+ '\n\tAvailability: {} %'.format(availability*100)
+
+    #     print(printStr)
+    #     return printStr
+        #True, printStr
+        '''{'Availability': availability, 'statusCodes': status_code.most_common(), 'maxRT': max_RT,
+                      'minRT:': min_RT, 'avgRT': avg_RT}'''
+    def get_stats(self, timeframe, database_name):
+        ''' Computes the statistiques of some metrics over a defined timeframe '''
+
+        # we calculate the start date we will take into consideration to compute the statistiques
+        # The start datetime = actual datetime - timeframe
         now = datetime.datetime.now()
         time_frame = datetime.timedelta(0, timeframe)
         t = now - time_frame
 
         selectData = (self.URL, t)
 
-        query_result = select_values(database_name, "monitoring_table", selectData)
+        # From our database "database_name", we select the data which is into the timeframe we considered
+        select_result = select_values(database_name, "monitoring_table", selectData)
 
-        if query_result is None:
-            printStr += "No data available"
-            return False, {} # "No data available"
+        # it there is no data on the selection result, then we can't compute the statistiques
+        if select_result is None:
+            return False, "No data available"
             '''False, {}'''
 
-        availabilities = Counter([element[2] for element in query_result if element[2] is not None])
-        status_code = Counter([element[3] for element in query_result])
-        response_times = [element[4] for element in query_result if element[4] is not None]
+        # if the selection result is not null, we compute the statistiques of the element we select
+        availabilities = Counter([element[2] for element in select_result if element[2] is not None])
+        status_code = Counter([element[3] for element in select_result])
+        response_times = [element[4] for element in select_result if element[4] is not None]
 
         availability = availabilities[True] / sum(availabilities.values())
-        max_RT = max(response_times, default=float('inf'))
+        max_RT = max(response_times)
         min_RT = min(response_times)
         avg_RT = sum(response_times) / len(response_times)
         time_date = datetime.datetime.now()
-        # print("availabilities : ", availabilities)
-        # print("availabilities[True] :", availabilities[True])
-        # print("availability :", availability)
 
+        # Save on the table 'alerts_table' on our database if there is a detected alert and when it's recovered
         if timeframe == 120: # 2 minutes
-            if availability < 0.8:
+            if availability < 0.8 and self.isDown == False:
+            # When the availability of a website is < 0.8, then we should save on the data base an alert message
                 self.isDown = True
                 messageType = "alert"
 
@@ -107,6 +227,7 @@ class Website():
                 insert_values(database_name, "alerts_table", values)
 
             if availability >= 0.8 and self.isDown == True:
+            # Once the availability is >= 0.8, we save on our database a recovery message
                 self.isDown = False
                 messageType = "recovery"
 
@@ -117,28 +238,29 @@ class Website():
         insert_values(database_name, "stats_table", values)
 
 
-        printStr += '\n\033[4;36m Past {} minutes:\033[0m'.format(timeframe/60) + \
+        printStr = '\n Past {} minutes:'.format(timeframe/60) + \
     '\n\tMax/Avg/Min response time: {:.4f}/{:.4f}/{:.4f} s'.format(max_RT, avg_RT, min_RT) + \
     '\n\tResponse counts: {}'.format(status_code.most_common())+ '\n\tAvailability: {} %'.format(availability*100)
 
-        print(printStr)
         return True, printStr
-        '''{'Availability': availability, 'statusCodes': status_code.most_common(), 'maxRT': max_RT,
-                      'minRT:': min_RT, 'avgRT': avg_RT}'''
 
     def checkAlerts(self, database_name):
-        alert_thread = threading.Timer(10 ,self.checkAlerts,args=[database_name])
+        '''Checks every 10seconds if we have some alert messages on the table "alerts_table" on our database'''
+        alert_thread = threading.Timer(10,self.checkAlerts,args=[database_name])
         alert_thread.start()
+
         query_result = select_values(database_name, "alerts_table", (self.URL,))
 
+        # if there is no Alert or Recovery message on the database, we return nothing
         if query_result is None:
-            return False, ''
+            return
+        # if there is an Alert or Recovery message on our alerts_table, we add it to the variable self.alertStr where we'll stock all the history of alert messages
         else:
-            self.alertStr = ""
+            alertStr = ""
             for element in query_result:
                 if element[4] == 'alert':
-                    self.alertStr += '\n\033[4;93m [ALERT MESSAGE !!] \033[0m '+ 'The website {} is DOWN '.format(self.URL) + ' at {}'.format(element[1]+'\t Availability : {} %'.format(element[2]*100))
+                    alertStr += '\n[ALERT MESSAGE !!] '+ 'The website {} is DOWN '.format(self.URL) + ' at {}'.format(element[1]+'\t Availability : {} %'.format(element[2]*100))
                 elif element[4] == 'recovery':
-                    self.alertStr += '\n\033[4;93m [RECOVERY MESSAGE !!] \033[0m '+ 'The website {} is RECOVERED '.format(self.URL) + ' at {}'.format(element[1]+'\t Availability : {} %'.format(element[2]*100))
-                print(self.alertStr)
-            return True, self.alertStr
+                    alertStr += '\n[RECOVERY MESSAGE !!] '+ 'The website {} is RECOVERED '.format(self.URL) + ' at {}'.format(element[1]+'\t Availability : {} %'.format(element[2]*100))
+            return alertStr
+
