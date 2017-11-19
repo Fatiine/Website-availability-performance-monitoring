ce518b3a67899d8a5e08a9a82694de37ded9bd33
Fatine BOUJNOUNI <fatine.boujnouni@gmail.com>
2017-11-19 04:56:35 +0100

    test.py : Update

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ import logging
 from database import create_tables, drop_tables
 from monitor import Monitor
 from website import Website
-
+from termcolor import colored
 # Simple Mock server with Flask
 app = Flask(__name__)
 
@@ -43,78 +43,114 @@ def test_alert_logic():
     monitor.run_monitor('test.db')
 
     # Start server 
-    print("Starting the server...")
+    print(colored("Starting the server...",'green'))
     server = Process(target=app.run, kwargs={'port':2000})
     server.start()
+
+    time.sleep(1)
+
     stats = monitor.statsPrinter('monitor.db', 10, 1) 
     alerts = monitor.alertsPrinter('monitor.db', 10) 
     monitor.alertsHist = ""
     print(stats)
-    print(alerts)
+    print(colored(alerts,'red'))
 
-    time.sleep(5)
+    time.sleep(10)
     stats = monitor.statsPrinter('monitor.db', 10, 1)
     alerts = monitor.alertsPrinter('monitor.db', 10)
     monitor.alertsHist = "" 
     print(stats)
-    print(alerts)
+    print(colored(alerts,'red'))
 
-    time.sleep(5)
-    stats = monitor.statsPrinter('monitor.db', 10, 1) 
+    time.sleep(10)
+    stats = monitor.statsPrinter('monitor.db', 10, 1)
     alerts = monitor.alertsPrinter('monitor.db', 10)
     monitor.alertsHist = "" 
     print(stats)
-    print(alerts)
+    print(colored(alerts,'red'))
+
+    time.sleep(10)
+    stats = monitor.statsPrinter('monitor.db', 10, 1)
+    alerts = monitor.alertsPrinter('monitor.db', 10)
+    monitor.alertsHist = "" 
+    print(stats)
+    print(colored(alerts,'red'))
+
+    time.sleep(10)
+    stats = monitor.statsPrinter('monitor.db', 10, 1)
+    alerts = monitor.alertsPrinter('monitor.db', 10)
+    monitor.alertsHist = "" 
+    print(stats)
+    print(colored(alerts,'red'))
+
+
 
 
     #Shut down the server
-    print("Shutting down the server ...")
+    print(colored("Shutting down the server ...",'yellow'))
     server.terminate()
     server.join()
     stats = monitor.statsPrinter('monitor.db', 10, 1) 
     alerts = monitor.alertsPrinter('monitor.db', 10)
     monitor.alertsHist = "" 
     print(stats)
-    print(alerts)
+    print(colored(alerts,'red'))
 
-    time.sleep(5)
+    time.sleep(10)
     stats = monitor.statsPrinter('monitor.db', 10, 1) 
     alerts = monitor.alertsPrinter('monitor.db', 10)
     monitor.alertsHist = "" 
     print(stats)
-    print(alerts)
+    print(colored(alerts,'red'))
 
-    time.sleep(5)
-    stats = monitor.statsPrinter('monitor.db', 10, 1) 
-    alerts = monitor.alertsPrinter('monitor.db', 10)
-    monitor.alertsHist = "" 
-    print(stats)
-    print(alerts)
+    
 
     # Restart the server
-    print("Restarting the server...")
+    print(colored("Re-starting the server...",'green'))
     server = Process(target=app.run, kwargs={'port':2000})
     server.start()
+ 
     stats = monitor.statsPrinter('monitor.db', 10, 1) 
     alerts = monitor.alertsPrinter('monitor.db', 10)
     monitor.alertsHist = "" 
     print(stats)
-    print(alerts)
+    print(colored(alerts,'red'))
 
-    time.sleep(5)
+    time.sleep(10)
     stats = monitor.statsPrinter('monitor.db', 10, 1) 
     alerts = monitor.alertsPrinter('monitor.db', 10)
     monitor.alertsHist = "" 
     print(stats)
-    print(alerts)
+    print(colored(alerts,'red'))
 
-    time.sleep(5)
+    time.sleep(10)
     stats = monitor.statsPrinter('monitor.db', 10, 1) 
     alerts = monitor.alertsPrinter('monitor.db', 10)
     monitor.alertsHist = "" 
     print(stats)
-    print(alerts)
+    print(colored(alerts,'red'))
 
-    print("Shutting down the server ...")
+    time.sleep(10)
+    stats = monitor.statsPrinter('monitor.db', 10, 1) 
+    alerts = monitor.alertsPrinter('monitor.db', 10)
+    monitor.alertsHist = "" 
+    print(stats)
+    print(colored(alerts,'red'))
+
+    time.sleep(10)
+    stats = monitor.statsPrinter('monitor.db', 10, 1) 
+    alerts = monitor.alertsPrinter('monitor.db', 10)
+    monitor.alertsHist = "" 
+    print(stats)
+    print(colored(alerts,'red'))
+
+    time.sleep(10)
+    stats = monitor.statsPrinter('monitor.db', 10, 1) 
+    alerts = monitor.alertsPrinter('monitor.db', 10)
+    monitor.alertsHist = "" 
+    print(stats)
+    print(colored(alerts,'red'))
+
+    print(colored("Shutting down the server ...",'yellow'))
     server.terminate()
     server.join()
